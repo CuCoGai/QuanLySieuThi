5ba3f83a85ca7567bf75a1354a62dcc744d326b8
thuyanhh <amyhuyendieu@gmail.com>
2017-06-05 12:04:00 +0700

    Revert "ql khách hàng"
    This reverts commit 17f75f439e8f3430d9613547c4af577983a16b7a.

--- QuanLySieuThi/QuanLySieuThi/View/frmDoiTac.cs
+++ QuanLySieuThi/QuanLySieuThi/View/frmDoiTac.cs
@@ -75,7 +75,6 @@ namespace QuanLySieuThi.View
                 dt.SdtNguoiDaiDien = this.textBoxSDTNgDD.Text;
             if (this.textBoxStatus.Text.Length != 0)
                 dt.Status = Int32.Parse(textBoxStatus.Text);
-            db.SaveChanges();
 
         }
 
